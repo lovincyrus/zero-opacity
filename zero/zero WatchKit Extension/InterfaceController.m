823946e832fac348fb24ecfbabf04bfc1a23627b
lovincyrus <hello@lovincyrus.com>
2017-02-10 00:41:07 -0800

    iOS and watchOS successfully send data and trigger haptic feedback

--- zero/zero WatchKit Extension/InterfaceController.m
+++ zero/zero WatchKit Extension/InterfaceController.m
@@ -11,54 +11,52 @@
 
 
 @interface InterfaceController() <WCSessionDelegate>
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *replyLabel;
 
-// reportBtn
-//@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *reportBtn;
+@property (assign) int counter;
+
+//@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *reportButton;
+//@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *speedLabel;
 
 // session
-@property (strong, nonatomic) WCSession *session;
-
+//@property (strong, nonatomic) WCSession *session;
 @end
 
 
 @implementation InterfaceController
 
--(instancetype)init {
-    self = [super init];
 
-    // check if a session is supported
-    if (self) {
-        if ([WCSession isSupported]) {
-            self.session = [WCSession defaultSession];
-            self.session.delegate = self;
-            [self.session activateSession];
-        }
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configuring and activating a session
+    if(WCSession.isSupported){
+        WCSession* session = WCSession.defaultSession;
+        session.delegate = self;
+        [session activateSession];
+        NSLog(@"InterfaceController.m WCSession is supported");
+
     }
-    return self;
+
+    self.counter = 1;
+    [self setTitle:[NSString stringWithFormat:@"%i",_counter]];
 }
 
 
 
 
-// activeationDidCompleteWithState
 - (void)session:(WCSession *)session
 activationDidCompleteWithState:(WCSessionActivationState)activationState
           error:(NSError *)error {
+    NSLog(@"Session did complete with state");
 }
-
-
-
-//
-//- (void)awakeWithContext:context {
-//    [super awakeWithContext:context];
-//    NSLog(@"Passed %@",context);
-//}
 
 
 
 
 - (void)willActivate {
-// This method is called when watch view controller is about to be visible to user
+    // This method is called when watch view controller is about to be visible to user
     [super willActivate];
     NSLog(@"%@ willActivate (HANDS UP)", self);
 }
@@ -75,23 +73,138 @@ activationDidCompleteWithState:(WCSessionActivationState)activationState
 
 
 
-- (IBAction)reportPressed {
-    [self sendReport:@"Report"];
-    NSLog(@"reportPressed");
+
+- (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *, id> *)message replyHandler:(void(^)(NSDictionary<NSString *, id> *replyMessage))replyHandler {
+    if(message){
+        NSString* command = [message objectForKey:@"request"];
+        [self.replyLabel setText:command];
+
+        NSString* otherCounter = [message objectForKey:@"counter"];
+        NSDictionary* response = @{@"response" : [NSString stringWithFormat:@"Message %@ received.",otherCounter]} ;
+
+        if (replyHandler != nil) replyHandler(response);
+    }
 }
 
 
 
 
-// send report to reportLabel
--(void)sendReport:(NSString *)report {
-    NSDictionary *applicationDict = @{@"report":report};
-    [self.session updateApplicationContext:applicationDict error:nil];
+-(void)packageAndSendMessage:(NSDictionary*)request {
+    // check session
+    if(WCSession.isSupported){
+        WCSession* session = WCSession.defaultSession;
+        session.delegate = self;
+        [session activateSession];
 
-    NSLog(@"sendReport");
+        if(session.reachable) {
+            [session sendMessage:request
+                    replyHandler:
+             ^(NSDictionary<NSString *,id> * __nonnull replyMessage) {
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     NSLog(@"--- replyHandler called --- %@",replyMessage);
+                     NSDictionary* message = replyMessage;
+                     NSString* response = message[@"response"];
+
+                        // Haptic Types
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeSuccess];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeNotification];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeDirectionUp];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeDirectionDown];
+                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeFailure];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeRetry];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeStart];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeStop];
+//                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeClick];
+
+
+                     if(response)
+                         [self.replyLabel setText:response];
+                     else
+                         [self.replyLabel setText:@"nil"];
+                 });
+             }
+                    errorHandler:^(NSError * __nonnull error) {
+
+                    // Failure Haptic
+                     [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeFailure];
+
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            [self.replyLabel setText:error.localizedDescription];
+                        });
+                    }
+             ];
+        }
+        else {
+            [self.replyLabel setText:@"Session Not reachable"];
+        }
+
+    }
+    else {
+        [self.replyLabel setText:@"Session Not Supported"];
+    }
+
+
+
+
 
 }
 
 
+
+
+
+
+
+
+
+/**
+ Standard WatchKit delegate
+ */
+-(void)sessionWatchStateDidChange:(nonnull WCSession *)session {
+    if(WCSession.isSupported){
+        WCSession* session = WCSession.defaultSession;
+        session.delegate = self;
+        [session activateSession];
+    }
+}
+
+
+
+#pragma mark Button Actions
+- (IBAction)sendMessageButtonPressed {
+    [self.replyLabel setText:@"Sending..."];
+
+    self.counter++;
+    [self setTitle:[NSString stringWithFormat:@"%i",_counter]];
+
+    NSDictionary* message = @{@"request":[NSString stringWithFormat:@"Message %d from the Phone",self.counter] ,@"counter":[NSString stringWithFormat:@"%d",self.counter]};
+
+    [self packageAndSendMessage:message];
+
+}
+
+
+
+
+- (IBAction)yesButtonPressed {
+    [self.replyLabel setText:@"Sending Yes..."];
+
+    self.counter++;
+    [self setTitle:[NSString stringWithFormat:@"%i",_counter]];
+
+    [self packageAndSendMessage:@{@"request":@"Yes",@"counter":[NSString stringWithFormat:@"%i",_counter]}];
+
+}
+
+
+
+- (IBAction)noButtonPressed {
+    [self.replyLabel setText:@"Sending No..."];
+
+    self.counter++;
+    [self setTitle:[NSString stringWithFormat:@"%i",_counter]];
+
+    [self packageAndSendMessage:@{@"request":@"No",@"counter":[NSString stringWithFormat:@"%i",_counter]}];
+}
 
 @end
--- zero/zero/ReportViewController.m
+++ zero/zero/ReportViewController.m
@@ -11,44 +11,70 @@
 
 @interface ReportViewController () <WCSessionDelegate>
 
-@property (weak, nonatomic) IBOutlet UILabel *reportLabel;
+@property (weak, nonatomic) IBOutlet UILabel *replyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *counterLabel;
+@property (assign) int counter;
 
 @end
 
 @implementation ReportViewController
 
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    if ([WCSession isSupported]) {
-        WCSession *session = [WCSession defaultSession];
+
+    // Configuring and activating a session
+    if(WCSession.isSupported){
+        WCSession* session = WCSession.defaultSession;
         session.delegate = self;
         [session activateSession];
+        NSLog(@"ReportViewController.m WCSession is supported");
     }
+
+
+    self.counter = 1;
+    self.counterLabel.text = [NSString stringWithFormat:@"%i",_counter];
 }
 
 
 
 
+/**
+ 
+ Called when the session prepares to stop communicating with the current Apple Watch.
+
+**/
+- (void)sessionDidBecomeInactive:(WCSession *)session {
+    NSLog(@"Session did become Inactive");
+}
+
+
+
+
+
+
+/**
+
+ Called after all data from the previous session has been delivered and communication with the Apple Watch has ended.
+
+**/
 - (void)sessionDidDeactivate:(WCSession *)session {
-    // Begin the activation process for the new Apple Watch.
     [[WCSession defaultSession] activateSession];
+
     NSLog(@"Session did deactivate");
 }
 
 
 
 
-- (void)sessionDidBecomeInactive:(WCSession *)session {
-    NSLog(@"Session did become Inative");
-}
 
+/**
+ 
+ Called when the activation of a session finishes.
 
-
-
-- (void)session:(WCSession *)session activationDidCompleteWithState:(WCSessionActivationState)activationState
+**/
+- (void)session:(WCSession *)session
+activationDidCompleteWithState:(WCSessionActivationState)activationState
           error:(NSError *)error {
     NSLog(@"Session did complete with state");
 }
@@ -56,27 +82,147 @@
 
 
 
-- (void)session:(nonnull WCSession *)session didReceiveApplicationContext:(nonnull NSDictionary<NSString *,id> *)applicationContext {
-    NSString *report = [applicationContext objectForKey:@"report"];
+- (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *, id> *)message replyHandler:(void(^)(NSDictionary<NSString *, id> *replyMessage))replyHandler {
+    NSLog(@"didReceiveMessage with replyHandler");
 
-    //Use this to update the UI instantaneously (otherwise, takes a little while)
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self.reportLabel setText:[NSString stringWithFormat:@"Label: %@", report]];
-    });
+    if(message){
+        NSString* command = [message objectForKey:@"request"];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.replyLabel setText:command];
+        });
+
+        NSString* otherCounter = [message objectForKey:@"counter"];
+        NSDictionary* response = @{@"response" : [NSString stringWithFormat:@"Message %@ received.",otherCounter]} ;
+
+        if (replyHandler != nil) replyHandler(response);
+    }
+}
+
+
+/**
+ Standard WatchKit delegate
+ */
+-(void)sessionWatchStateDidChange:(nonnull WCSession *)session
+{
+
+    if(WCSession.isSupported){
+        WCSession* session = WCSession.defaultSession;
+        session.delegate = self;
+        [session activateSession];
+
+
+        if(session.reachable){
+            NSLog(@"session.reachable");
+        }
+
+        if(session.paired){
+            if(session.isWatchAppInstalled){
+                if(session.watchDirectoryURL != nil){
+                }
+            }
+        }
+    }
+}
+
+
+-(void)packageAndSendMessage:(NSDictionary*)request {
+    if(WCSession.isSupported){
+
+        WCSession* session = WCSession.defaultSession;
+        session.delegate = self;
+        [session activateSession];
+
+        if(session.reachable) {
+            [session sendMessage:request replyHandler: ^(NSDictionary<NSString *,id> * __nonnull replyMessage) {
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     NSLog(@"--- replyHandler called --- %@",replyMessage);
+                     NSDictionary* message = replyMessage;
+                     NSString* response = message[@"response"];
+
+                     if(response)
+                         [self.replyLabel setText:response];
+                     else
+                         [self.replyLabel setText:@"nil"];
+                 });
+             }
+
+                    errorHandler:^(NSError * __nonnull error) {
+
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            NSLog(@"%@",error.localizedDescription);
+                            [self.replyLabel setText:error.localizedDescription];
+                        });
+                    }
+             ];
+        }
+        else {
+            [self.replyLabel setText:@"Session Not reachable"];
+        }
+
+    }
+    else {
+        [self.replyLabel setText:@"Session Not Supported"];
+    }
 }
 
 
 
 
 
-/*
- #pragma mark - Navigation
 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+#pragma mark Button Actions
+- (IBAction)SendMessageButtonPressed:(id)sender {
+    [self.replyLabel setText:@"Sending Message ..."];
 
+    NSDictionary* message = @{@"request":[NSString stringWithFormat:@"Message %d from the Phone",self.counter] ,@"counter":[NSString stringWithFormat:@"%d",self.counter]};
+
+    self.counter++;
+    self.counterLabel.text = [NSString stringWithFormat:@"%i",_counter];
+
+    //Send message
+    [self packageAndSendMessage:message];
+
+}
+
+
+//- (IBAction)sendMessageButtonPressed {
+//    [self.replyLabel setText:@"Sending..."];
+//
+//    self.counter++;
+//    [self setTitle:[NSString stringWithFormat:@"%i",_counter]];
+//
+//    NSDictionary* message = @{@"request":[NSString stringWithFormat:@"Message %d from the Phone",self.counter] ,@"counter":[NSString stringWithFormat:@"%d",self.counter]};
+//
+//    [self packageAndSendMessage:message];
+//    
+//}
+
+
+
+
+- (IBAction)yesButtonPressed:(id)sender {
+    [self.replyLabel setText:@"Sending Yes..."];
+
+    self.counter++;
+    self.counterLabel.text = [NSString stringWithFormat:@"%i",_counter];
+
+    //Build message and send
+    [self packageAndSendMessage:@{@"request":@"Yes",@"counter":[NSString stringWithFormat:@"%i",_counter]}];
+}
+
+
+
+
+- (IBAction)noButtonPressed:(id)sender {
+
+    [self.replyLabel setText:@"Sending No..."];
+
+    self.counter++;
+    self.counterLabel.text = [NSString stringWithFormat:@"%i",_counter];
+
+
+    //Build message and send
+    [self packageAndSendMessage:@{@"request":@"No",@"counter":[NSString stringWithFormat:@"%i",_counter]}];
+}
 @end
